1f93f3e37afc10e80a124c0bb8164a0f3f644a08
Sathish <sathishkrishnan369@gmail.com>
2026-08-29 22:38:05 +0530

    Added implementation in main.py for graph

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from dotenv import load_dotenv
-import os
+from langgraph.graph import MessagesState, StateGraph, END
+from nodes import run_agent_reasoning, tool_node
+from langchain_core.messages import HumanMessage
 
 load_dotenv()
+
+AGENT_REASON= "agent_reason"
+ACT= "act"
+LAST=-1
+
+def should_continue(state: MessagesState) -> str:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue,{END: END, ACT:ACT})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 
 
 def main():
     print("Hello from langgraph-course!")
-
+    res = app.invoke({"messages": [HumanMessage(content="Where Attapadi is located? List it and triple the pincode of the place")]})
+    print(res["messages"][LAST].content)
 
 if __name__ == "__main__":
     main()
