59105c88ab9442ed2a646a529d4a938d37c9a427
Sathish <sathishkrishnan369@gmail.com>
2026-09-03 10:19:33 +0530

    Implementation of reflexion agent

--- main.py
+++ main.py
@@ -1,12 +1,60 @@
 from dotenv import load_dotenv
-import os
-
 load_dotenv()
+from typing import Literal
+from langchain_core.messages import AIMessage,ToolMessage
+from langgraph.graph import START,END, MessagesState, StateGraph
+from chains import first_responder, revisor
+from tool_executor import execute_tools
+
+MAX_ITERATIONS = 2
 
 
-def main():
-    print("Hello from langgraph-course!")
+def draft_node(state:MessagesState):
+    """Draft a initial Response"""
+    response = first_responder.invoke({"messages": state["messages"]})
+    return {"messages" :[response]}
+
+def revise_node(state:MessagesState):
+    """Revise the answer based on the tool result"""
+    response = revisor.invoke({"messages": state["messages"]})
+    return {"messages" :[response]}
+
+def event_loop(state:MessagesState) -> Literal["execute_tools",END]:
+    """Determine whether to continue or end based on iteration count."""
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state["messages"])
+    if count_tool_visits > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
 
 
-if __name__ == "__main__":
-    main()
+builder = StateGraph(MessagesState)
+builder.add_node("draft", draft_node)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revise_node)
+
+
+builder.add_edge(START, "draft")
+builder.add_edge("draft", "execute_tools")
+builder.add_edge( "execute_tools", "revise")
+
+builder.add_conditional_edges("revise", event_loop, ["execute_tools", END])
+
+graph  = builder.compile()
+print(graph.get_graph().draw_mermaid())
+
+res = graph.invoke(
+    {
+        "messages": [
+            {
+                "role" : "user",
+                "content": "Write about volleyball player and the workouts they do, list the workout that helps in improving vertical jump"
+            }
+        ]
+    }
+)
+
+#extract the final answer from the last message with tool calls
+last_message = res["messages"][-1]
+if isinstance(last_message, AIMessage) and last_message.tool_calls:
+    print(last_message.tool_calls[0]["args"]["answer"])
+print(res)
